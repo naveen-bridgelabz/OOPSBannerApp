be74d13d565e07717aa50545213209560fda9bb1
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-01-29 16:57:30 +0530

    Added UC-2 OOP Banner

---
+++ src/main/java/org/example/OOPBanner.java
@@ -0,0 +1,24 @@
+/*
+* OOP Banner UC2 - OOP Banner Application
+*
+*  This Class created to visually demonstrate the OOP in the console using ASCII art using asterisks (*) and space.
+*  Without using Loops.
+*
+* @author : Naveen Kumar
+* @version : 2
+*
+* */
+
+package org.example;
+
+public class OOPBanner {
+    public static void main(String[] args) {
+        System.out.println("     ***     "  + "     ***     " + " *****     " + "   ****     ");
+        System.out.println("   **   **   "  + "   **   **   " + " **  ***   " + " ***   ***  ");
+        System.out.println("  **     **  "  + "  **     **  " + " **    **  " + "   ***      ");
+        System.out.println(" **       ** "  + " **       ** " + " **  ***   " + "     ***    ");
+        System.out.println("  **     **  "  + "  **     **  " + " *****     " + "        *** ");
+        System.out.println("   **   **   "  + "   **   **   " + " **        " + " ***   ***  ");
+        System.out.println("     ***     "  + "     ***     " + " **        " + "    ****    ");
+    }
+}
