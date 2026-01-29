973591a621bef92127fd45a5e0bebe42e37d5c66
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-01-29 17:55:28 +0530

    Added OOP Banner using String.join()

---
+++ src/main/java/org/example/RenderBanner.java
@@ -0,0 +1,23 @@
+/*
+* OOP Banner UC3 - Render Banner App
+*
+* This Branch features rendering OOPS Banner using String.join()
+*
+* @author : Naveen Kumar
+* @version : 3.0
+* */
+
+package org.example;
+
+public class RenderBanner {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ","     ****      "  + "     ****      " + " *******     " + "   *****     "));
+        System.out.println(String.join(" ","   ***  ***    "  + "   ***  ***    " + " ***   ***   " + "***     ***  "));
+        System.out.println(String.join(" "," ***      ***  "  + " ***      ***  " + " ***    ***  " + " ***      "));
+        System.out.println(String.join(" ","***        *** "  + "***        *** " + " ***   ***   " + "    ***    "));
+        System.out.println(String.join(" "," ***      ***  "  + " ***      ***  " + " *******     " + "       *** "));
+        System.out.println(String.join(" ","   ***  ***    "  + "   ***  ***    " + " ***         " + "***     ***  "));
+        System.out.println(String.join(" ","     ****      "  + "     ****      " + " ***         " + "   *****    "));
+
+    }
+}
