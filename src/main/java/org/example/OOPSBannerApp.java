ade3d39e47c99e68afbd82a2d7ec17cfec18243b
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-01-29 16:18:16 +0530

    UC 1 Print OOPS Added

---
+++ src/main/java/org/example/OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/*
+* OOP Banner UC1 - OOP Banner Application
+*
+*Java Class to Display OOP in the console in the process of learning ObjectOriented Programming
+*
+* @author : Naveen Kumar
+* @version : 1.0
+*
+* */
+
+
+package org.example;
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
